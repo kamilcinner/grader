d089970c9673efc4d36e6277a91d0448eb7d5b23
Kamil Cinner <kamil.cinner@gmail.com>
2022-10-22 13:57:40 +0200

    [WEBAPP] Theming refactor

--- grader-webapp/src/app/features/configuration/configuration.component.ts
+++ grader-webapp/src/app/features/configuration/configuration.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { ConfigurationService } from './configuration.service';
+import { combineLatest, map } from 'rxjs';
 
 @Component({
   selector: 'app-configuration',
   templateUrl: './configuration.component.html',
-  styleUrls: ['./configuration.component.scss']
+  styleUrls: ['./configuration.component.scss'],
 })
 export class ConfigurationComponent implements OnInit {
+  readonly vm$ = combineLatest([this.configurationService.getAllGrades()]).pipe(map(([grades]) => ({ grades })));
 
-  constructor() { }
+  constructor(private readonly configurationService: ConfigurationService) {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
